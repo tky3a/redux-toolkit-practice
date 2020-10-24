0086c158f185953c6a46ddbf86f453931a4d8589
tky3a <takayamac@icloud.com>
2020-10-25 00:34:15 +0900

    関数コンポーネントでredux-toolkit

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import { SafeAreaView, View, Text } from "react-native"
 import React from "react"
 import { Provider } from "react-redux"
-import { Divider, Colors } from "react-native-paper"
+
 import CounterBasic from "./src/components/CounterBasic"
 import CounterRTKwithSliceConnect from "./src/components/CounterRTKwithSliceConnect"
+import CounterRTKwithSliceReactHook from "./src/components/CounterRTKwithSliceReactHook"
+
 import store from "./src/store"
-// const store = configureStore()
-const MyDivider = () => <Divider style={{ backgroundColor: Colors.red900 }} />
+
 export default function () {
   return (
     <SafeAreaView
@@ -18,11 +19,13 @@ export default function () {
     >
       <Provider store={store}>
         <View>
-          <MyDivider />
-          <CounterBasic />
-          <MyDivider />
+          <View>
+            <CounterBasic />
+          </View>
           <CounterRTKwithSliceConnect />
-          <MyDivider />
+          <View>
+            <CounterRTKwithSliceReactHook />
+          </View>
         </View>
       </Provider>
     </SafeAreaView>
---
+++ src/components/CounterRTKwithSliceReactHook.js
@@ -0,0 +1,23 @@
+import React, { useReducer } from "react"
+import { Text, View, Button } from "react-native"
+import reducer, { increment, decrement } from "../modules/countsSlice"
+
+const initialState = 0
+
+export default CounterRTKwithSliceReactHook = () => {
+  const [counts, dispatch] = useReducer(reducer, initialState)
+  console.log("counts", counts)
+  return (
+    <View>
+      <Button
+        title="+ RTKwithSliceReactHook"
+        onPress={() => dispatch(increment)}
+      />
+      <Text style={{ textAlign: "center" }}>Count: {counts}</Text>
+      <Button
+        title="- RTKwithSliceReactHook"
+        onPress={() => dispatch(decrement)}
+      />
+    </View>
+  )
+}
